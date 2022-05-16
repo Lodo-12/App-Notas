84e8c81809e3223c8f619e50ff2c18f118b342d7
Lodo-12 <101215396+Lodo-12@users.noreply.github.com>
2022-05-16 22:18:51 +0200

    dale

---
+++ crud_notas/lib/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(color:Colors.indigo ),
+          ),
+        ),
+      ],
+    );
+  }
+}
